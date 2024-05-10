00bd0da1cf10f2664bb2b95ab24a82bec582e24d
Hidenori Matsubayashi <Hidenori.Matsubayashi@gmail.com>
2024-05-10 20:38:36 +0900

    common: update the output log format of log macro

--- src/common/logger.h
+++ src/common/logger.h
@@ -10,18 +10,18 @@
     evisor::printf(format "\n", ##__VA_ARGS__); \
   }
 #else
-#define _LOG_COMMON(level, format, ...)                          \
-  {                                                              \
-    evisor::printf("%s[%s(%d)]: ", (level), __FILE__, __LINE__); \
-    evisor::printf(format "\n", ##__VA_ARGS__);                  \
+#define _LOG_COMMON(level, format, ...)                         \
+  {                                                             \
+    evisor::printf("%s[%s(%d)] ", (level), __FILE__, __LINE__); \
+    evisor::printf(format "\n", ##__VA_ARGS__);                 \
   }
 #endif  // NDEBUG
 
-#define PANIC(format, ...)                           \
-  {                                                  \
-    _LOG_COMMON("[PANIC]: ", format, ##__VA_ARGS__); \
-    while (1)                                        \
-      ;                                              \
+#define PANIC(format, ...)                                 \
+  {                                                        \
+    _LOG_COMMON("[eVisor][PANIC]", format, ##__VA_ARGS__); \
+    while (1)                                              \
+      ;                                                    \
   }
 #define LOG_ERROR(format, ...) \
   _LOG_COMMON("[eVisor][ERROR]", format, ##__VA_ARGS__)
